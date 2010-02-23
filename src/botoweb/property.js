6964d712bce8c75742699123fb9cc39b22f21b33
ipaterson <devnull@localhost>
2010-02-23 12:33:49 -0500

    Added new Property class for uniform representation and access of properties, factory creation of object properties, reduced copied data, and elimination of problems with data getting assigned to model properties. Model and object properties are still very similar to allow them to be used interchangeably, but they are distinct.

---
+++ src/botoweb/property.js
@@ -0,0 +1,125 @@
+/**
+ * Structures properties and creates a factory for construction of object data
+ * properties.
+ *
+ * @author Ian Paterson
+ */
+
+/**
+ * Represents a property of a model but also returns a constructor which is
+ * custom built based on the property type.
+ *
+ * @constructor
+ */
+botoweb.Property = function(name, type, perm, model, opt) {
+	opt = opt || {};
+
+	var model_prop = this;
+
+	/**
+	 * Creates a new object
+	 *
+	 * @constructor
+	 */
+	this.instance = function (data) {
+		var self = this;
+
+		self.data = data || {};
+
+		self.meta = model_prop.meta;
+
+		$.each(['equals','is_type','val'], function () {
+			self[this] = model_prop[this];
+		});
+	};
+
+	switch (type) {
+		case 'list':
+			this.equals = function(other) {
+				if (!this.is_type(other.type))
+					return false;
+
+				for (var i in this.data) {
+					if (i >= other.data.length || this.data != other.data[i])
+						return false;
+				}
+
+				return true;
+			};
+			this.val = function() {
+				return this.data;
+			};
+			break;
+
+		case 'complexType':
+			this.equals = function(other) {
+				if (!this.is_type(other.type))
+					return false;
+
+				for (var i in this.data) {
+					if (i >= other.data.length
+						|| this.data[i].name != other.data[i].name
+						|| this.data[i].value != other.data[i].value
+					)
+						return false;
+				}
+
+				return true;
+			};
+			break;
+
+		case 'reference':
+			this.equals = function(other) {
+				if (!this.is_type(other.type))
+					return false;
+
+				return this.data.id == other.data.id
+					&& this.data.href == other.data.href;
+			};
+			break;
+
+		case 'query':
+			this.equals = function(other) {
+				return false;
+			};
+			break;
+	}
+
+	this.equals = this.equals || function (other) {
+		return this.data.value == other.data.value;
+	};
+
+	this.val = this.val || function () {
+		return this.data.value || this.data;
+	};
+
+	this.is_type = function () {
+		var type = this.meta.type;
+
+		for (var i = 0; i < arguments.length; i++) {
+			if (type == arguments[i])
+				return type;
+		}
+
+		return false;
+	};
+
+	this.meta = {
+		_DEBUG_PROPERTY_INSTANCE: 1,
+		name: name || '',
+		type: type || '',
+		perm: perm || [],
+		model: model,
+		read: false,
+		write: false
+	};
+
+	if ($.inArray('read', this.meta.perm) >= 0)
+		this.meta.read = true;
+
+	if ($.inArray('write', this.meta.perm) >= 0)
+		this.meta.write = true;
+
+	// Copy any optional data
+	$.extend(this.meta, opt);
+};
