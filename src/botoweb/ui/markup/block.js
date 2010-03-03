563cb7d53e9f9d488a15031e5decf2845f34e283
ipaterson <devnull@localhost>
2010-03-03 18:14:56 -0500

    Optimized Block parsing to skip selectors which we know have no results for much better performance when the same template is parsed multiple times. Added clone method to pass along the optimization data to a new Block.

--- src/botoweb/ui/markup/block.js
+++ src/botoweb/ui/markup/block.js
@@ -4,31 +4,73 @@
  * @author Ian Paterson
  */
 
-botoweb.ui.markup.Block = function (node, parent) {
+botoweb.ui.markup.Block = function (node, opt) {
+	if (!opt) opt = {};
+
 	var markup = botoweb.ui.markup;
 	var self = this;
 
 	this.node = node;
-	this.model = null;
-	this.obj = null;
+	this.model = opt.model;
+	this.obj = opt.obj;
+	this.parent = opt.parent;
+	this.nested = [];
 
-	if (parent)
-		this.model = parent.model;
+	this.skip_markup = opt.skip_markup || {};
+	this.nested_sel = opt.nested_sel;
+
+	/**
+	 * Passes parsing optimization data along to a new block created from node.
+	 * The node should of course be the same as the node that was used to create
+	 * this block, otherwise the optimization data is probably invalid.
+	 *
+	 * @param {DOMNode} node The node to build the Block on.
+	 * @param {Object} opt Same options as Block constructor.
+	 */
+	this.clone = function (node, opt) {
+		if (!opt) opt = {};
+
+		opt.skip_markup = this.skip_markup;
+		opt.nested_sel = this.nested_sel;
+
+		return new botoweb.ui.markup.Block(node, opt);
+	};
+
+	if (this.parent && !this.model)
+		this.model = this.parent.model;
 
 	if (node.attr(markup.prop.model)) {
 		this.model = botoweb.env.models[node.attr(markup.prop.model)];
 	}
 
+	/**
+	 * If the parsing routine has not been marked to skip, runs the parsing
+	 * function. If the parsing function returns
+	 *
+	 * @private
+	 */
+	function parse (str, fnc) {
+		if (self.skip_markup[str])
+			return;
+
+		// If the parser returns false, skip it next time
+		if (fnc(self) === false)
+			self.skip_markup[str] = 1;
+	}
+
 	// Do not allow nested blocks to interfere
-	markup.remove_nested(this);
+	parse('nested', markup.remove_nested);
 
 	// Parse stuff in the order specified
 	$.each(['condition', 'trigger', 'attribute_list', 'attribute', 'editing_tools', 'link'], function () {
-		markup.parse[this](self);
+		parse(this, markup.parse[this]);
 	});
 
 	// Add nested blocks again
 	markup.restore_nested(this);
 
-	markup.parse.relation(this);
+	// Parse stuff in the order specified
+	$.each(['relation','search'], function () {
+		parse(this, markup.parse[this]);
+	});
 };
