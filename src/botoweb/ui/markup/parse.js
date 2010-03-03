bdce9b199781e8321c4c8cc9dae026ef1ef51788
ipaterson <devnull@localhost>
2010-03-03 18:22:35 -0500

    Added boolean return value to all parsing methods to tell the caller whether the selector matched at least once.

--- src/botoweb/ui/markup/parse.js
+++ src/botoweb/ui/markup/parse.js
@@ -13,7 +13,11 @@
 		 * corresponding condition function returns false.
 		 */
 		condition: function (block) {
+			var matches = false;
+
 			self.find(block.node, 'condition', function (val, prop) {
+				matches = true;
+
 				if (val in botoweb.env.cfg.conditions){
 					if(botoweb.env.cfg.conditions[val](block.obj, this) === false)
 						this.remove();
@@ -23,88 +27,156 @@
 					this.removeAttr(prop);
 				}
 			});
+
+			return matches;
 		},
 
 		/**
 		 * Parse triggers and execute them.
 		 */
 		trigger: function (block) {
+			var matches = false;
+
 			self.find(block.node, 'trigger', function (val, prop) {
+				matches = true;
+
 				if (val in botoweb.env.cfg.triggers)
 					botoweb.env.cfg.triggers[val](block.obj, this);
 				else
 					this.removeAttr(prop);
 			});
+
+			return matches;
 		},
 
 		/**
 		 * Parse attributes.
 		 */
 		attribute: function (block) {
+			var matches = false;
+
 			if (!block.model && !block.obj)
 				return;
 
 			self.find(block.node, 'attribute', function() {
-
+				matches = true;
 			});
+
+			return matches;
 		},
 
 		/**
 		 * Parse links.
 		 */
 		link: function (block) {
+			var matches = false;
+
 			if (!block.model && !block.obj)
 				return;
 
 			self.find(block.node, 'link', function(val, prop) {
+				matches = true;
+
 				this.removeAttr(prop);
 				this.attr('href', '#');
 			});
+
+			return matches;
 		},
 
 		/**
 		 * Parse attribute lists.
 		 */
 		attribute_list: function (block) {
+			var matches = false;
+
 			if (!block.model && !block.obj)
 				return;
 
 			self.find(block.node, 'attribute_list', function() {
-				new botoweb.ui.widgets.AttributeList(this);
+				matches = true;
+
+				new botoweb.ui.widget.AttributeList(this);
 			});
+
+			return matches;
 		},
 
 		/**
 		 * Parse relation blocks.
 		 */
 		relation: function (block) {
+			var matches = false;
+
 			if (!block.model && !block.obj)
 				return;
 
 			self.find(block.node, 'relation', function() {
-
+				matches = true;
 			});
+
+			return matches;
 		},
 
 		/**
 		 * Parse date times.
 		 */
 		date_time: function (block) {
+			var matches = false;
+
 			self.find(block.node, 'date_time', function() {
-				new botoweb.ui.widgets.DateTime(this);
+				matches = true;
+
+				new botoweb.ui.widget.DateTime(this);
 			});
+
+			return matches;
 		},
 
 		/**
-		 * Parse triggers and execute them.
+		 * Add editing tools for models and objects.
 		 */
 		editing_tools: function (block) {
+			var matches = false;
+
 			if (!block.model && !block.obj)
 				return;
 
 			self.find(block.node, 'editing_tools', function() {
-				new botoweb.ui.widgets.EditingTools(this, block.model, (this.attr(self.prop.attributes) || ''));
+				matches = true;
+				new botoweb.ui.widget.EditingTools(this, block.model, (this.attr(self.prop.attributes) || ''));
 			});
+
+			return matches;
+		},
+
+		/**
+		 * Parse search blocks.
+		 */
+		search: function (block) {
+			var matches = false;
+
+			self.find(block.node, 'search', function() {
+				matches = true;
+
+				new botoweb.ui.widget.Search(this);
+			});
+
+			return matches;
+		},
+
+		/**
+		 * Parse search result blocks.
+		 */
+		search_results: function (block) {
+			var matches = false;
+
+			self.find(block.node, 'search_results', function() {
+				matches = true;
+				new botoweb.ui.widget.SearchResults(this, block.model);
+			});
+
+			return matches;
 		}
 	};
 })();
