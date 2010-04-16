d6846dd6185179d457bf3d567a936b8a2d659a07
ipaterson <devnull@localhost>
2010-04-16 13:48:28 -0400

    Fixed missing param on error handling

--- src/botoweb/ldb/sync.js
+++ src/botoweb/ldb/sync.js
@@ -145,13 +145,40 @@ botoweb.ldb.sync = {
 
 		console.log("=====> " + model.name + " <=====");
 		console.log("Refresh: " + refresh);
-		console.log("last_update: " + localStorage['last_update_' + model.name]);
+
+		if (refresh) {
+			model.all(processor, options);
+		}
+		else {
+			botoweb.ldb.dbh.transaction(function (txn) {
+				alert('SELECT MAX(prop_sys_modstamp) AS last_update FROM ' + botoweb.ldb.model_to_table(model));
+				txn.executeSql(
+					'SELECT MAX(prop_sys_modstamp) AS last_update FROM ' + botoweb.ldb.model_to_table(model),
+					function (txn, results) {
+						console.warn(results);
+						if (results.rows.length) {
+							var last_update = results.rows.item(0).last_update;
+							console.log("last_update: " + last_update);
+
+							model.query([['sys_modstamp', '>', last_update]], processor, options);
+						}
+						else
+							model.all(processor, options);
+					},
+					function () {
+						console.warn('bad');
+						model.all(processor, options);
+					}
+				);
+			});
+		}
+/*
 		if (!refresh && localStorage['last_update_' + model.name]) {
 			model.query([['sys_modstamp', '>', localStorage['last_update_' + model.name]]], processor, options);
 		} else {
 			model.all(processor, options);
 		}
-
+*/
 		// Although we may fetch multiple pages of results, these results are a
 		// snapshot of the current state, so the update time is now, not when
 		// the query ends.
